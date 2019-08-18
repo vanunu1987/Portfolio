fcfed6da7dc4d5574f5cacbbc76c13e1f8e4221d
Your Name <vanunu24@gmail.com>
2019-08-18 22:30:27 +0300

    Updates

---
+++ precache-manifest.44887dc4af11ea92482ce885110d3b79.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b51d871a2e4d441787d1b06723aeb0a",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "74011a98470b2fb3e3b4",
+    "url": "/react-gh-pages/static/css/main.534bf8a7.chunk.css"
+  },
+  {
+    "revision": "4b99f4a3e86b8e054e2d",
+    "url": "/react-gh-pages/static/js/2.3d32cb7c.chunk.js"
+  },
+  {
+    "revision": "74011a98470b2fb3e3b4",
+    "url": "/react-gh-pages/static/js/main.6701444e.chunk.js"
+  },
+  {
+    "revision": "87099879561f846a7c1c",
+    "url": "/react-gh-pages/static/js/runtime~main.1f4941a6.js"
+  },
+  {
+    "revision": "c21522c742ab9fe6e8413fb739e6b2f9",
+    "url": "/react-gh-pages/static/media/about1.c21522c7.png"
+  },
+  {
+    "revision": "e0ecf3eafc524f42c4b5267b27526eaa",
+    "url": "/react-gh-pages/static/media/about2.e0ecf3ea.png"
+  },
+  {
+    "revision": "d2f0233ee3b2ecf78e9d8bdf3d8ef2ca",
+    "url": "/react-gh-pages/static/media/about3.d2f0233e.png"
+  },
+  {
+    "revision": "df0bece02875d0d115d7b8463b512b06",
+    "url": "/react-gh-pages/static/media/about4.df0bece0.png"
+  },
+  {
+    "revision": "055cea53e9c81cfefc153fd44a8acd61",
+    "url": "/react-gh-pages/static/media/hatimg1.055cea53.png"
+  },
+  {
+    "revision": "0507a6242fe14872799c3b701ae7b642",
+    "url": "/react-gh-pages/static/media/hatimg2.0507a624.png"
+  },
+  {
+    "revision": "77541d65a1ac9b114d7817de1849926c",
+    "url": "/react-gh-pages/static/media/hatimg3.77541d65.png"
+  },
+  {
+    "revision": "c450df69f3276a44db7dfea12453a95f",
+    "url": "/react-gh-pages/static/media/hatimg4.c450df69.png"
+  },
+  {
+    "revision": "db47bd62461e111ab58c40185f9b81eb",
+    "url": "/react-gh-pages/static/media/port1.db47bd62.png"
+  }
+]);
