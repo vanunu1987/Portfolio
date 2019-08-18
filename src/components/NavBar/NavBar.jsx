8b249f653058790e9f2264ae256fbd051aa95a41
Your Name <vanunu24@gmail.com>
2019-08-19 00:14:58 +0300

    bug fixing 3

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = (props) =>{
     const tampletObj = {home:false,about:false,skills:false,portfolio:false,contact:false}
     const navList = []
     for (let cmp in props.components){
-    navList.push((<li key={cmp} onClick={()=>props.active({...tampletObj,[cmp]:true})} className={props.components[cmp]&&classes['active']}>{cmp.toUpperCase()}</li>))}
+    navList.push((<li key={cmp} onClick={()=>props.active({...tampletObj,[cmp]:true})} className={props.components[cmp]?classes['active']:''}>{cmp.toUpperCase()}</li>))}
     
     return(
     <div className={classes['nav-container']}>
